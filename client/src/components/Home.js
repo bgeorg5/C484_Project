978800abe7b3e546d802191b36ad73c805e36b35
E10 <edjsitnek@gmail.com>
2021-11-15 19:38:28 -0500

    added ejs as dependency, slightly improved home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,8 +5,10 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <img src={logo} alt="WeatherOrNot Logo" />;
-                <p>This is the home page</p>
+                <center>
+                    <img src={logo} alt="WeatherOrNot Logo" />;
+                    <h1>Welcome to WeatherOrNot!</h1>
+                </center>
             </div>
         );
     }
